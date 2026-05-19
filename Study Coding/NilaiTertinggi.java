1aba8d26dbaf1ebf80aa0a749af7689b5e4d20ed
ziee <keziasalsabilla518@gmail.com>
2026-05-19 08:10:41 +0700

    m9-n0.3-25410100008

---
+++ Study Coding/NilaiTertinggi.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class NilaiTertinggi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+System.out.print("Masukkan jumlah angka : ");
+    int jumlah = sc.nextInt();
+
+    int[] arr = new int[jumlah];
+
+for(int i = 0; i < arr.length; i++){
+System.out.print("Masukkan angka ke-" + (i+1) + " : ");
+    arr[i] = sc.nextInt();
+}
+
+System.out.println("Nilai terbesar = " + getNilaiMax(arr));
+}
+static int getNilaiMax(int[] arr){
+
+int max = arr[0];
+
+for(int i = 1; i < arr.length; i++){
+    if(arr[i] > max){
+    max = arr[i];
+    }
+}
+
+return max;
+    }
+}
